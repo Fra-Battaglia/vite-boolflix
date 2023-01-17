2b91299eb1ebc55c016f128de1592fbb2df7f303
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-01-17 17:33:22 +0100

    project setup done

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+	import { store } from './store.js'
+
+	export default {
+		data() {
+			return {
+				store
+			}
+		},
+	}
+</script>
+
+<template>
+	
+</template>
+
+<style lang="scss">
+	@use './components/styles/partials/variables';
+	@use './components/styles/partials/mixins';
+
+	@use './components/styles/generals.scss';
+</style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const store = reactive ({
+	api_key: '5ab214ce791eb47bb350cb86680be475',
+	api_movies: 'https://api.themoviedb.org/3/search/movie?api_key=5ab214ce791eb47bb350cb86680be475&language=en-US&query=',
+});
